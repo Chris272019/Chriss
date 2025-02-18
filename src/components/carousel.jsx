62e773e5f83363b967c41d1a69150d457c1c0af7
Chris <christiantan2719@gmail.com>
2025-02-18 18:24:20 +0800

    update

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './carousel.css';
 
-const images = [
-    'images/2.webp',
-    'images/3.jpg',
-    'images/1.jpg',
-    'images/4.jpg',
-    'images/5.jpg',
+// Import images directly
+import image1 from '../images/2.jpg';
+import image2 from '../images/3.jpg';
+import image3 from '../images/1.jpg';
+import image4 from '../images/4.jpg';
+import image5 from '../images/5.jpg';
 
-    // Add more images here if you have them
-];
+const images = [image1, image2, image3, image4, image5];
 
 const Carousel = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
