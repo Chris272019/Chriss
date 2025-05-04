a59d929e4244d953d76b76722a4c2585d057fdba
Chris <christiantan2719@gmail.com>
2025-05-04 12:12:56 +0800

    feat: add navigation bar to blog section

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,10 +1,27 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Link } from "react-router-dom"
 import "./Blog.css"
 
 function Blog() {
   return (
     <div className="blog-container">
-      <Outlet />
+      <nav className="blog-navbar">
+        <div className="nav-back">
+          <Link to="/" className="nav-link">← Back to Home</Link>
+        </div>
+        <div className="nav-days">
+          <Link to="/blog" className="nav-link">Home</Link>
+          <Link to="/blog/day1" className="nav-link">Day 1</Link>
+          <Link to="/blog/day2" className="nav-link">Day 2</Link>
+          <Link to="/blog/day3" className="nav-link">Day 3</Link>
+          <Link to="/blog/day4" className="nav-link">Day 4</Link>
+          <Link to="/blog/day5" className="nav-link">Day 5</Link>
+          <Link to="/blog/day6" className="nav-link">Day 6</Link>
+          <Link to="/blog/day7" className="nav-link">Day 7</Link>
+        </div>
+      </nav>
+      <div className="blog-content-wrapper">
+        <Outlet />
+      </div>
     </div>
   )
 }
