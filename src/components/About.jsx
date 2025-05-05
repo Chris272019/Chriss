46c17b382690a866a190affd3cc685474f783584
Chris <christiantan2719@gmail.com>
2025-05-05 14:23:13 +0800

    Fix image import path case sensitivity in About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import "./About.css"
 import SoundButton from "./SoundButton"
-import profileImage from "../images/Profile.png"
+import profileImage from "../images/profile.png"
 
 const About = () => {
   return (
